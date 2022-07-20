92f4493dd234b7843c28f6d977dad011f64f17b4
long <long.nq.hust@gmail.com>
2022-07-20 07:47:25 +0700

    done

--- APP/components/Main/Notify/Notify.js
+++ APP/components/Main/Notify/Notify.js
@@ -2,9 +2,43 @@ import {Text, View, ScrollView, TouchableOpacity} from 'react-native'
 import IconAntDesign from 'react-native-vector-icons/AntDesign';
 import IconFeather from 'react-native-vector-icons/Feather';
 import IconIon from 'react-native-vector-icons/Ionicons';
-
+import MQTT from 'sp-react-native-mqtt';
 export default function Notify(){
-    const Noti = ({type, title, content, datetime}) => {
+
+    // MQTT.createClient({
+    //     uri: 'mqtt://broker.hivemq.com:1883',
+    //     // clientId: 'your_client_id'
+    //   }).then(function(client) {
+      
+    //     client.on('closed', function() {
+    //       console.log('mqtt.event.closed');
+    //     });
+      
+    //     client.on('error', function(msg) {
+    //       console.log('mqtt.event.error', msg);
+    //     });
+      
+    //     client.on('message', function(msg) {
+    //       console.log('mqtt.event.message', msg);
+    //     });
+      
+    //     client.on('connect', function() {
+    //       console.log('connected');
+    //       client.subscribe('/notify', 0);
+    //     });
+      
+    //     client.connect();
+    //   }).catch(function(err){
+    //     console.log(err);
+    //   });
+
+
+    //  const [notifies, setNotifies] = useState(init);
+     // const init = [{type: 'danger', title: 'Danger!', content: 'Nhiệt độ phòng bếp lên quá cao, đã kích hoạt chế độ dập lửa', datetime: Date.now}]
+    
+
+
+      const Noti = ({type, title, content, datetime}) => {
         var icon, color='#C4C4C4';
         switch (type) {
             case 'danger':
@@ -46,6 +80,8 @@ export default function Notify(){
         <Noti type = 'warn' title={'Warning'} content={'Bạn đang để nhiệt độ điều hòa quá thấp, kiến nghị nâng nhiệt độ lên 26°C'} datetime={'16/07/2022 - 21:32PM'} ></Noti>
         <Noti  title={'Good morning!'} content={'Nhiệt độ trong phòng là 26°C, nhiệt độ ngoài trời hiện tại là 34°C, độ ẩm 70%, khả năng có mưa 80%'} datetime={'17/07/2022 - 06:32AM'} ></Noti>
         <Noti type = 'success' title={'Every thing is OK'} content={'Mọi thứ đang hoạt động ổn áp'} datetime={'18/07/2022 - 10:00AM'} ></Noti>
+        {/* {notifies.map(e => <Noti type={e.type} title ={e.title} content={e.content} datetime={e.datetime} ></Noti>)} */}
        </ScrollView>
     )
 }
+
